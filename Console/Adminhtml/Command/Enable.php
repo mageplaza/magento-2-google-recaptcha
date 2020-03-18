8b3e034282c1cc8cf5128bd0d87f154151041611
arthas78 <duongnm@mageplaza.com>
2020-03-18 10:11:11 +0700

    add console for captcha

---
+++ Console/Adminhtml/Command/Enable.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_GoogleRecaptcha
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+
+namespace Mageplaza\GoogleRecaptcha\Console\Adminhtml\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Mageplaza\GoogleRecaptcha\Helper\Data as HelperData;
+use Magento\Framework\App\Config\Storage\WriterInterface as ConfigWriter;
+use Magento\Framework\App\ScopeInterface as ScopeConfigInterface;
+
+class Enable extends Command
+{
+    /**
+     * @var HelperData
+     */
+    protected $helperData;
+
+    /**
+     * @var HelperData
+     */
+    protected $_configWriter ;
+
+    /**
+     * Enable constructor.
+     * @param HelperData $helperData
+     * @param ConfigWriter $configWriter
+     * @param null $name
+     */
+    public function __construct(
+        HelperData $helperData,
+        ConfigWriter $configWriter,
+        $name = null
+    ) {
+        $this->helperData = $helperData;
+        $this->_configWriter = $configWriter;
+
+        parent::__construct($name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('mpgooglerecaptcha:backend_login:enable')
+            ->setDescription('Enable backend captcha');
+
+        parent::configure();
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return mixed
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (!$this->helperData->isEnabled()) {
+            $output->writeln('Module is not enabled for your website.');
+        }
+        else if ($this->helperData->isCaptchaBackend()) {
+            $output->writeln('The captcha is enabled for your admin website.');
+        }
+        else {
+            $path = 'googlerecaptcha/backend/enabled';
+            $this->_configWriter->save($path, '1', $scope = ScopeConfigInterface::SCOPE_DEFAULT, $scopeId = 0);
+            $output->writeln('The captcha backend has been successfully enabled. Please run the flush cache command again');
+        }
+    }
+}
