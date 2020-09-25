fd62450b0552ad5dd190fdd45e59fa783b9d5c7b
Nguyen Huy Phuc <phucnh@mageplaza.com>
2020-09-25 16:53:44 +0700

    [2.4.0] Google reCaptcha Error 1: fix notification

--- view/frontend/web/js/captcha.js
+++ view/frontend/web/js/captcha.js
@@ -108,7 +108,7 @@ define([
                         var divCaptcha    = $('<div class="g-recaptcha"></div>');
                         var divAction     = $('.actions-toolbar');
                         var divError      = $('<div class="g-recaptcha-error"></div>');
-                        var checkBox      = $('<input type="checkbox" style="visibility: hidden" data-validate="{required:true}" class="mage-error">');
+                        var checkBox      = $('<input type="checkbox" style="display: none" data-validate="{required:true}" class="mage-error">');
 
                         divError.text($t('You need select captcha')).attr('style', 'display:none;color:red');
                         divCaptcha.attr('id', 'mp' + '_recaptcha_' + number);
